e292e1d3dd2dddb0f507ed0e92792d8cc5d5add0
Pavan Kumar <168668332+pavankumarpunugu@users.noreply.github.com>
2026-03-13 13:39:33 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -63,8 +63,11 @@ LOGOUT_REDIRECT_URL="/"
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
