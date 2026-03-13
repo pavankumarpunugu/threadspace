1484b5547268f146cd2d61e6ef1e2b848e5cf293
Pavan Kumar <168668332+pavankumarpunugu@users.noreply.github.com>
2026-03-13 13:02:48 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -21,11 +21,13 @@ INSTALLED_APPS=[
 
 MIDDLEWARE=[
 'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
 'django.contrib.sessions.middleware.SessionMiddleware',
 'django.middleware.common.CommonMiddleware',
 'django.middleware.csrf.CsrfViewMiddleware',
 'django.contrib.auth.middleware.AuthenticationMiddleware',
 'django.contrib.messages.middleware.MessageMiddleware',
+
 ]
 
 ROOT_URLCONF="core.urls"
